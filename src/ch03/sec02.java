04b9282852464c72acd2bb87a811136c9811f1ac
ssm512 <shinsang512@naver.com>
2026-04-13 20:28:30 +0900

    20260413 java prac ch03

--- src/ch03/sec02.java
+++ src/ch03/sec02.java
@@ -3,7 +3,41 @@ package ch03;
 public class sec02 {
 
 	public static void main(String[] args) {
-		// 
+		// Compare Operator Example
+		int		num1			=	10;
+		int		num2			=	20;
+		boolean		result1		=	(num1 == num2);
+		boolean		result2		=	(num1!=num2);
+		boolean		result3		=	(num1<num2);
+		System.out.println(result1);
+		System.out.println(result2);
+		System.out.println(result3);
+		
+		char		char1		=	'A';
+		char		char2		=	'B';
+		boolean		result4		=	(char1<char2);
+		System.out.println("result4 : " + result4);
+		
+		int			num3		=	1;
+		double		num4		=	1.0;
+		boolean 	result5		=	(num3==num4);
+		System.out.println("result5 : " + result5);
+		
+		float		num5		=	0.1f;
+		double		num6		=	0.1;
+		boolean		result6		=	(num5==num6);
+		boolean		result7		=	(num5==(float)num6);
+		System.out.println("result6 : " + result6);
+		System.out.println("result7 : " + result7);
+		
+		String		str1		=	"자바";
+		String		str2		=	"java";
+		boolean 	result8		=	(str1.equals(str2));
+		boolean		result9		=	(! str1.equals(str2));
+		System.out.println("result8 : " + result8);
+		System.out.println("result9 : " + result9);
+		
+		
 
 	} // main end
 
---
+++ src/ch03/sec03.java
@@ -0,0 +1,30 @@
+package ch03;
+
+public class sec03 {
+
+	public static void main(String[] args) {
+		// Logical Operator Example
+		int			charCode		=	'A';
+		if	((65<=charCode) & (charCode<=90)) {
+			System.out.println("대문자이군요");
+		} // if 문 end
+		if ((97<=charCode)&& (charCode <= 122)) {
+			System.out.println("소문자이군요");
+		} // if 문 end
+		if ((48<=charCode)&&(charCode<=57)) {
+			System.out.println("0~9 숫자이군요");
+		} // if 문 end
+		
+		// -------------------------------------------
+		int			value			=	6;
+		if	((value%2==0) | (value%3==0)) {
+			System.out.println("2 또는 3의 배수이군요.");
+			
+		boolean		result			=	(value%2==0) || (value%3==0);
+		if (!result) {
+			System.out.println("2또는 3의 배수가 아닌군요");
+		}// if 문 end
+		} // if 문 end
+	} // main end
+
+} // class sec03 end
