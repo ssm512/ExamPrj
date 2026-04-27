d466c95db1a06301bb2735b3d8b653792f40d3a2
ssm512 <shinsang512@naver.com>
2026-04-28 02:14:03 +0900

    이것이 자바다 homework commit

---
+++ src/ch06/Korean2.java
@@ -0,0 +1,16 @@
+package ch06;
+
+public class Korean2 {
+	// 인스턴스 final 필드 선언
+	final String nation = "대한민국";
+	final String ssn;
+	
+	//인스턴스 필드 선언
+	String name;
+	
+	// 생성자 선언
+	public Korean2(String ssn, String name) {
+		this.ssn = ssn;
+		this.name = name;
+	}
+} // class Korean2 end
---
+++ src/ch06/KoreanExample2.java
@@ -0,0 +1,23 @@
+package ch06;
+
+public class KoreanExample2 {
+
+	public static void main(String[] args) {
+		// 객체 생성 시 주민등록번호와 이름 전달
+		Korean2 k1 = new Korean2("123456-1234567", "김자바");
+		
+		// 필드값 읽기
+		System.out.println(k1.nation);
+		System.out.println(k1.ssn);
+		System.out.println(k1.name);
+
+		
+		// final 필드는 값을 변경할 수 없음
+		// k1.nation = "USA";
+		// k1.ssn = "123-12-1234";
+		
+		//  비 final 필드는 값 변경 가능
+		k1.name = "박자바";
+	} // main end
+
+} // class KoreanExample2 end
