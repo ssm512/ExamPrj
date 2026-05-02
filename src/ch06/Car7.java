3ba0185e3b51d9ace8d3c7270c9d1c8d60400f45
ssm512 <shinsang512@naver.com>
2026-05-02 23:48:16 +0900

    20260502 이것이자바다 commit

---
+++ src/ch06/Car7.java
@@ -0,0 +1,11 @@
+package ch06;
+
+import ch06.hankook.SnowTire;
+import ch06.kumho.AllSeasonTire;
+
+public class Car7 {
+	ch06.hankook.Tire tire1	=	new ch06.hankook.Tire();
+	ch06.kumho.Tire	tire2	=	new ch06.kumho.Tire();
+	SnowTire tire3	=	new	SnowTire();
+	AllSeasonTire tire4 = new AllSeasonTire();
+}
---
+++ src/ch06/kumho/AllSeasonTire.java
@@ -0,0 +1,5 @@
+package ch06.kumho;
+
+public class AllSeasonTire {
+
+}
